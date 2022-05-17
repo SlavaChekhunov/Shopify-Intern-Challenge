4d688a5ed3ce05018dd04ff12b9ab194d3f9e6f3
SlavaChekhunov <slava97@my.yorku.ca>
2022-05-17 00:50:30 -0400

    added firebase functionality so that poems remain with page refreshes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
+//modules
+import {  useState, useEffect } from "react";
+import firebase from "./firebase";
+import { getDatabase, ref, onValue, push, remove } from "firebase/database";
+//styles
+import './App.css';
+
+
 //Pseudocode:
-//Create a form for text prompts and attach an event listener to the submit button
+//Create a form for text prompts and attach an event listener to it.
 //Submitting the form will send the prompt to the OpenAi API.
 //Results will be displayed in a list, sorted from the newest to the latest.
 //Each result should include the original prompt and a response from the API.
 // API-KEY: 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
 
-
-
-import './App.css';
-import CreatePost from './components/CreatePost';
-import {  useState } from "react";
-
-
 const App = () => {
-  const OPENAI_SECRET = "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";
- const [posts, setPosts] = useState([]);
+  //API key
+  const OPENAI_SECRET = "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";
+  //responses is the state that displays the response from the API call.
+ const [responses, setResponses] = useState('');
+ //prompt is the state that displays the prompt the user typed onto the page.
   const [prompt, setPrompt] = useState('');
+  //the message is for error handling, that displays an error if there is no response from the API.
   const [message, setMessage] = useState('');
+  //container is what is being pushed into the firebase.
+  const [container, setContainer] = useState([]);
 
 
- const handleSubmit = async (event) => {
-  event.preventDefault();
-  
-  const response = await fetch(
+ useEffect(() => {
+   fetch(
     "https://api.openai.com/v1/engines/text-curie-001/completions",
     {
       method: "POST",
@@ -39,20 +44,53 @@ const App = () => {
         presence_penalty: 0.0,
       }),
     }
-  );
-  const responseJson = await response.json();
-  if (response.status === 200) {
-    setPrompt('')
-    setPosts(responseJson.choices[0].text);
-    setMessage("Data received successfully");
-  } else {
-    setMessage("Some error occurred");
-  }
-  console.log("json", responseJson);
+  )
+  .then((response) => response.json())
+  .then((jsonResult) => {
+    setResponses(jsonResult.choices[0].text);
+    })
+    .catch((error) => {
+      setMessage(error);
+    })
+}, [responses]);
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const database = getDatabase(firebase);
+  const dbRef = ref(database);
+  push(dbRef, { dataFromAPI: responses, userPrompt: prompt });
+  setPrompt("");
+};;
+
+useEffect(() => {
+  const database = getDatabase(firebase);
+  const dbRef = ref(database);
+  onValue(dbRef, (response) => {
+    // here we're creating a variable to store the new state we want to introduce to our app
+    const newState = [];
+    // here we store the response from our query to Firebase inside of a variable called data.
+    const data = response.val();
+    for (let key in data) {
+      newState.push({
+        key: key,
+        response: data[key].dataFromAPI,
+        prompt: data[key].userPrompt,
+      });
+    }
+    setContainer(newState.reverse());
+  });
+}, []);
+
+ const handleRemoveContainer = (containerId) => {
+   // here we create a reference to the database
+   const database = getDatabase(firebase);
+   const dbRef = ref(database, `/${containerId}`);
+   remove(dbRef);
  };
-    
+
     return (
       <div className="App">
+        <h1>Fun with AI</h1>
         <form onSubmit={handleSubmit}>
           <label htmlFor="newPost">Enter your Prompt</label>
           <textarea
@@ -66,11 +104,21 @@ const App = () => {
           <button>Submit</button>
         </form>
         <div className="message">{message ? <p>{message}</p> : null}</div>
-        <div className="text">{posts}</div>
-        <div className="text">{prompt}</div>
+        <div className="container">
+          {container.map((container) => {
+            return (
+              <div key={container.key}>
+                <p>Prompt: {container.prompt}</p>
+                <p>Response: {container.response}</p>
+                <button onClick={() => handleRemoveContainer(container.key)}>
+                  Remove
+                </button>
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
-        
-}
+  }
       
-    export default App;
+  export default App;
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDGe7e9BAiIB2dfiYo_g4iwXjs34StagP4",
+  authDomain: "shopify-intern-challenge-263d8.firebaseapp.com",
+  projectId: "shopify-intern-challenge-263d8",
+  storageBucket: "shopify-intern-challenge-263d8.appspot.com",
+  messagingSenderId: "36099912372",
+  appId: "1:36099912372:web:41a0a86e1ed0d31ca85de1",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
